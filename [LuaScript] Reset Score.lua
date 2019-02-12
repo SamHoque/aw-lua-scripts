ef6731e64e5cc0ea54c80cbf017392f82dce1ea3
unknown <princerabbz@gmail.com>
2019-02-13 02:57:37 +0300

    Updated to use player_death event

--- [LuaScript] Reset Score.lua
+++ [LuaScript] Reset Score.lua
@@ -1,6 +1,6 @@
 callbacks.Register('FireGameEvent', function(e)
     local en = e:GetName();
-    if (en ~= "round_start") then return; end
+    if (en ~= "player_death") then return; end
     local i = entities.GetLocalPlayer():GetIndex();
     local k = entities.GetPlayerResources():GetPropInt("m_iKills", i);
     local d = entities.GetPlayerResources():GetPropInt("m_iDeaths", i);
