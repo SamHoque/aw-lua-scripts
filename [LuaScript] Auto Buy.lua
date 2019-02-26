25d151ab3ad0b67e8b42da6ba619f249a7beb3ab
unknown <princerabbz@gmail.com>
2019-02-26 18:26:41 +0300

    Updated Script

--- [LuaScript] Auto Buy.lua
+++ [LuaScript] Auto Buy.lua
@@ -20,78 +20,98 @@ local armors = {
     { "Kevlar Vest + Helmet", "vest", "vesthelm" };
 };
 local granades = {
-    { "Off", nil, nil };
+    { "None", nil, nil };
     { "Grenade", "hegrenade", nil };
     { "Flashbang", "flashbang", nil };
     { "Smoke Grenade", "smokegrenade", nil };
     { "Decoy Grenade", "decoy", nil };
     { "Molotov | Incindiary Grenade", "molotov", "incgrenade" };
 };
-local mainWindow = gui.Window("rab_autobuy", "Auto Buy", 200, 200, 250, 530);
-local autoBuyGroup = gui.Groupbox(mainWindow, "Settings", 15, 15, 220, 456);
+local mainWindow = gui.Window("rab_autobuy", "Auto Buy", 200, 200, 250, 554);
+local autoBuyGroup = gui.Groupbox(mainWindow, "Settings", 15, 15, 220, 480);
 local enabled = gui.Checkbox(autoBuyGroup, "rab_autobuy_masterswitch", "Enabled Auto Buy", false);
 local printLogs = gui.Checkbox(autoBuyGroup, "rab_autobuy_printlogs", "Print Logs To Aimware Console", false);
+local concatCommand = gui.Checkbox(autoBuyGroup, "rab_autobuy_concat", "Concact Buy Command", false);
+concatCommand:SetValue(true);
 local primaryWeaponSelection = gui.Combobox(autoBuyGroup, "rab_autobuy_primary_weapon", "Primary Weapon", primaryWeapons[1][1], primaryWeapons[2][1], primaryWeapons[3][1], primaryWeapons[4][1], primaryWeapons[5][1]);
 local secondaryWeaponSelection = gui.Combobox(autoBuyGroup, "rab_autobuy_secondary_weapon", "Secondary Weapon", secondaryWeapons[1][1], secondaryWeapons[2][1], secondaryWeapons[3][1], secondaryWeapons[4][1]);
 local armorSelection = gui.Combobox(autoBuyGroup, "rab_autobuy_armor", "Armor", armors[1][1], armors[2][1], armors[3][1]);
+armorSelection:SetValue(2);
 local granadeSlot1 = gui.Combobox(autoBuyGroup, "rab_autobuy_grenade_slot_1", "Grenade Slot #1", granades[1][1], granades[2][1], granades[3][1], granades[4][1], granades[5][1], granades[6][1]);
+granadeSlot1:SetValue(1);
 local granadeSlot2 = gui.Combobox(autoBuyGroup, "rab_autobuy_grenade_slot_2", "Grenade Slot #2", granades[1][1], granades[2][1], granades[3][1], granades[4][1], granades[5][1], granades[6][1]);
+granadeSlot2:SetValue(3);
 local granadeSlot3 = gui.Combobox(autoBuyGroup, "rab_autobuy_grenade_slot_3", "Grenade Slot #3", granades[1][1], granades[2][1], granades[3][1], granades[4][1], granades[5][1], granades[6][1]);
+granadeSlot3:SetValue(5);
 local granadeSlot4 = gui.Combobox(autoBuyGroup, "rab_autobuy_grenade_slot_4", "Grenade Slot #4", granades[1][1], granades[2][1], granades[3][1], granades[4][1], granades[5][1], granades[6][1]);
+granadeSlot4:SetValue(2);
 local taser = gui.Checkbox(autoBuyGroup, "rab_autobuy_taser", "Buy Taser", false);
 local defuseKit = gui.Checkbox(autoBuyGroup, "rab_autobuy_defusekit", "Buy Defuse Kit", false);
 gui.Text(mainWindow, "Auto Buy - Made By Rab(SamzSakerz#4758)");
 
-local function buy(wat)
-    if (wat == nil) then return end;
-    if (printLogs) then
-        print('Bought x1 ' .. wat);
-    end;
-    client.Command('buy "' .. wat .. '";', true);
+local function getSingleTableItem(selection, table)
+    return table[selection:GetValue() + 1][2];
 end
 
-
-local function buyTable(table)
-    for i, j in pairs(table) do
-        buy(j);
-    end;
+local function getMultiTableItems(seletion, table)
+    local table = table[seletion:GetValue() + 1];
+    return { table[2], table[3] };
 end
 
-local function buyWeapon(selection, table)
-    local selection = selection:GetValue();
-    local weaponToBuy = table[selection + 1][2];
-    buy(weaponToBuy);
-end
-
-local function buyGrenades(selections)
-    for k, selection in pairs(selections) do
-        local selection = selection:GetValue();
-        local grenadeTable = granades[selection + 1];
-        buyTable({ grenadeTable[2], grenadeTable[3] });
+local function insertToTableNonNull(tableToInsertTo, table1)
+    for i = 1, #table1 do
+        local item = table1[i];
+        if (item ~= nil) then
+            table.insert(tableToInsertTo, item);
+        end
     end
+end
+
+local function inserToTableBool(tableToInserTo, bool, itemToInsert)
+    if (bool:GetValue()) then
+        table.insert(tableToInserTo, itemToInsert);
+    end
+end
+
+local function buy(items, concat)
+    local buyCommand = '';
+    for i = 1, #items do
+        local item = items[i];
+        if (concat) then
+            buyCommand = buyCommand .. 'buy "' .. item .. '"; ';
+        else
+            if (printLogs:GetValue()) then
+                print('Bought x1 ' .. item);
+            end;
+            client.Command('buy "' .. item .. '";', true);
+        end;
+    end;
+    if (buyCommand ~= '') then
+        if (printLogs:GetValue()) then
+            print('Bought x' .. #items .. ' items');
+        end;
+        client.Command(buyCommand);
+    end;
 end
 
 callbacks.Register('FireGameEvent', function(e)
-    if (enabled:GetValue() ~= true) then return end;
-    local localPlayer = entities.GetLocalPlayer();
-    local en = e:GetName();
-    if (localPlayer == nil or en ~= "player_spawn") then return end;
-    local userIndex = client.GetPlayerIndexByUserID(e:GetInt('userid'));
-    local localPlayerIndex = localPlayer:GetIndex();
-    if (userIndex ~= localPlayerIndex) then return end;
-    buyWeapon(primaryWeaponSelection, primaryWeapons);
-    buyWeapon(secondaryWeaponSelection, secondaryWeapons);
-    local armorSelected = armorSelection:GetValue();
-    local armorTable = armors[armorSelected + 1];
-    buyTable({ armorTable[2], armorTable[3] });
-    if (defuseKit:GetValue()) then
-        buy('defuser');
-    end
-    if (taser:GetValue()) then
-        buy('taser');
-    end
-    buyGrenades({ granadeSlot1, granadeSlot2, granadeSlot3, granadeSlot4 });
+    local lp, en, ui = entities.GetLocalPlayer(), e:GetName(), client.GetPlayerIndexByUserID(e:GetInt('userid'));
+    local lpi = lp:GetIndex();
+    if (enabled:GetValue() ~= true or lp == nil or en ~= "player_spawn" or ui ~= lpi) then return
+    end;
+    local stuffToBuy = {};
+    table.insert(stuffToBuy, getSingleTableItem(primaryWeaponSelection, primaryWeapons))
+    table.insert(stuffToBuy, getSingleTableItem(secondaryWeaponSelection, secondaryWeapons));
+    insertToTableNonNull(stuffToBuy, getMultiTableItems(armorSelection, armors));
+    inserToTableBool(stuffToBuy, defuseKit, 'defuser');
+    inserToTableBool(stuffToBuy, taser, 'taser');
+    insertToTableNonNull(stuffToBuy, getMultiTableItems(granadeSlot1, granades));
+    insertToTableNonNull(stuffToBuy, getMultiTableItems(granadeSlot2, granades));
+    insertToTableNonNull(stuffToBuy, getMultiTableItems(granadeSlot3, granades));
+    insertToTableNonNull(stuffToBuy, getMultiTableItems(granadeSlot4, granades));
+    buy(stuffToBuy, concatCommand:GetValue());
 end);
+
 
 callbacks.Register("Draw", function()
     if input.IsButtonPressed(gui.GetValue("msc_menutoggle")) then
