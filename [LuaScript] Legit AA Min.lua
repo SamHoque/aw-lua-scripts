9c8de12a92df6b80007fabfa6678a4839e5a65c8
LynxKik <princerabbz@gmail.com>
2019-03-22 19:30:09 +0300

    Initial Release

---
+++ [LuaScript] Legit AA Min.lua
@@ -0,0 +1,15 @@
+--[=====[
+     Legit Anti-Aim Made by Rab(SamzSakerz#4758)
+             __        __
+            |__)  /\  |__)
+            |  \ /~~\ |__)
+       __   __   ___      ___  ___
+      /  ` |__) |__   /\   |  |__
+      \__, |  \ |___ /~~\  |  |___
+               ___  __           ___     ___
+|  | |__|  /\   |  /__`    |\ | |__  \_/  |
+|/\| |  | /~~\  |  .__/    | \| |___ / \  |
+
+--]=====]
+
+local a=draw.CreateFont('Marlett',45,700)local b=true;local c=gui.Reference('RAGE','Main','Anti-aim main')local d=gui.Checkbox(c,'rab_legitaa_enable','Legit AA',false)local e=gui.Keybox(c,'rbot_legitaa_right','Right Key',0)local f=gui.Keybox(c,'rbot_legitaa_left','Left Key',0)local function g(h)if h~=0 then return input.IsButtonDown(h)end end;local function i()if g(f:GetValue())then b=true end;if g(e:GetValue())then b=false end end;local function j()local k,l=draw.GetScreenSize()local m=b and 2 or 3;gui.SetValue('rbot_antiaim_stand_desync',m)gui.SetValue('rbot_antiaim_move_desync',m)draw.SetFont(a)draw.Color(249,0,0,255)draw.Text(k/2+(b and 60 or-100),l/2,b and'4'or'3')draw.TextShadow(k/2+(b and 60 or-100),l/2,b and'4'or'3')end;local function n()local o={{'at_targets',0},{'autodir',0},{'move_pitch_real',0},{'stand_pitch_real',0},{'stand_real',1},{'stand_velocity',250}}for p,q in ipairs(o)do gui.SetValue('rbot_antiaim_'..q[1],q[2])end end;callbacks.Register('Draw',function()if d:GetValue()then i()j()n()end end)
